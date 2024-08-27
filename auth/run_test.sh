6f6eacf40d1f4a488f64b632663dbf065f2b8643
saisahoo01 <143263503+saisahoo01@users.noreply.github.com>
2024-08-27 22:56:18 +0530

    Update run_test.sh

--- auth/run_test.sh
+++ auth/run_test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-BUILD_TAG='build'
+BUILD_TAG=${1:-'build'}
 TEST_IMAGE=auth_tests:${BUILD_TAG}
 
 docker build -t ${TEST_IMAGE} --build-arg BUILDTAG=${BUILD_TAG} -f auth/Dockerfile_tests .
